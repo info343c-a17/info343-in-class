5d81bc904dd65a60cf73a0ebb4333b2456bcf717
Jacob A. Morris <jacob.morris@gmail.com>
2017-10-26 23:12:40 -0700

    created initial task list

--- tasks/js/index.js
+++ tasks/js/index.js
@@ -1,4 +1,15 @@
 // @ts-check
 "use strict";
 
+let state = {
+    tasks: [
+        {title: "Create tasks as li elements"},
+        {title: "Render the tasks as a unordered list on the page"},
+        {title: "Learn Event Handling"},
+        {title: "Add new task to the list object and then display newly added item"},
+        {title: "Add functionality to mark items as done"},
+        {title: "Implement the purge feature to clear out completed tasks"}
+    ]
+};
+
 //TODO: let's implement a dynamic task list!
