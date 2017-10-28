4814bf2c32376e44fd5548802d39e460ab55cc8d
Jacob A. Morris <jacob.morris@gmail.com>
2017-10-27 17:33:40 -0700

    updated ajax

--- ajax/js/index.js
+++ ajax/js/index.js
@@ -12,3 +12,54 @@ const ERROR_ALERT = document.querySelector("#error-alert");
 //TODO: fetch from Location API to get lat/lng
 //and then fetch Weather API for that lat/lng.
 //Render results to the page, or handle errors
+
+function handleError(err) {
+    console.error(err);
+    ERROR_ALERT.textContent = err.message;
+    ERROR_ALERT.classList.remove("d-none");
+}
+
+function handleResponse(response) {
+    console.log("got response", response);
+    if (response.ok) {
+        return response.json();
+    } else {
+        return response.text()
+            .then(function(message) {
+                throw new Error(message);
+            });
+    }
+}
+
+function fetchWeather(data) {
+    console.log(data);
+    let latitude = data.latitude;
+    let longitude = data.longitude;
+    let url = WEATHER_API.replace("{latitude}", latitude)
+                .replace("{longitude}", longitude);
+    return fetch(url);
+}
+
+function renderWeather(data) {
+    console.log(data);
+    let span = document.querySelector("#temp");
+    span.textContent = data.main.temp;
+
+    if (data.weather && data.weather[0]) {
+        let conditions = data.weather[0];
+        document.querySelector("#description")
+            .textContent = conditions.description;
+        let img = /** @type {HTMLImageElement} */ (document.querySelector("#weather-icon"));
+        img.src = ICON_URL.replace("{icon}", conditions.icon);
+        img.alt = conditions.description;
+    }
+}
+
+fetch(LOCATION_API)
+    .then(handleResponse)
+    .then(fetchWeather)
+    .then(handleResponse)
+    .then(renderWeather)
+    .catch(handleError);
+
+console.log("next line after fetch executes immediately!");
