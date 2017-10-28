7dc6823f4fb1ccff0c0ab7bd5be58a92f716c528
Dave Stearns <dlsinfo@uw.edu>
2017-10-28 11:30:58 -0700

    starter files for es6 day

---
+++ es6/src/index.js
@@ -0,0 +1,9 @@
+// @ts-check
+"use strict";
+
+//TODO: create classes for Task,
+//TaskList, Button, TaskForm, and App.
+//Each of these classes should have
+//a render() method that renders their
+//data as HTML elements.
+
